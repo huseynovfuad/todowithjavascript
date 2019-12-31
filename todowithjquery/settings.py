34231a3715bbc37333941b7b62d84d37574d1672
huseynovfuad <55555038+huseynovfuad@users.noreply.github.com>
2019-12-31 14:38:48 +0400

    Update settings.py

--- todowithjquery/settings.py
+++ todowithjquery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(tptmamj08z8^qyyjre!k9s2lzog47=f21z%as@ptjrhdaf(^&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
